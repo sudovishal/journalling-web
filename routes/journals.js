0725a445199dc47a2fd0bd4b20c51937d33112e5
Vishal Shinde <vs.vishalshinde@outlook.com>
2023-09-19 21:24:08 +0530

    Dockerized the Web Applicationdocker compose up

--- routes/journals.js
+++ routes/journals.js
@@ -4,7 +4,7 @@ router.get('/', async (req,res) => {
   try {
     const userId = req.user.id;
     const journals = await Journal.find({userId : userId}).sort({ createdAt: 'desc' })
-    res.render('dashboard',{name : 'Vishal', journals : journals});
+    res.render('dashboard',{ journals : journals});
   } catch (error) {
    console.log(error);
    res.render('error',{error : error}) 
