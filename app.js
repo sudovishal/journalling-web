ecb8b4adc0c2df278d620a4d5f2132d659c30fcb
Kaust44 <110822757+Kaust44@users.noreply.github.com>
2023-10-20 08:40:19 +0000

    share-link

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const viewJournalRoute = require('./routes/view-journal.js')
 const deleteJournalRoute = require('./routes/delete-journal.js')
 const editJournalRoute = require('./routes/edit-journal.js')
 const profileChange = require('./routes/profile-change.js')
-const passwordReset = require('./routes/password-reset.js')
+const shareableLink = require('./routes/public-link');
 // Configure body-parser middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -39,11 +39,12 @@ app.use('/signup', signupRoute);
 
 
 app.use('/journals',authenticateToken, journalRoute);
-app.use('/forgot-password', passwordReset);
 app.use('/journals/', authenticateToken, createJournalRoute)
 app.use('/',logoutRoute)
-app.use('/journals', authenticateToken, viewJournalRoute)
+app.use('/journals', authenticateToken);
+app.use('/journals', viewJournalRoute);
 app.use('/journals', authenticateToken ,deleteJournalRoute)
 app.use('/journals', authenticateToken, editJournalRoute)
 app.use('/', authenticateToken, profileChange)
+app.use('/', shareableLink)
 app.listen(3000);
---
+++ routes/public-link.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { model } = require('mongoose');
+const Journal = require('../models/journal')
+router.post('/journals/:slug/visibility', async (req, res) => {
+    try {
+        const slug = req.params.slug;
+        const isPublic = req.body.isPublic === 'on'; // Check if the checkbox is checked
+        const journal = await Journal.findOne({slug : slug});
+      if (!journal) {
+        res.status(404).send('Journal not found');
+      }  else {
+         // Update the visibility of the journal
+         journal.isPublic = isPublic;
+         await journal.save();
+         res.redirect('/journals'); 
+        } 
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+    });
+module.exports = router
