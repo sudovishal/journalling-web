fca74450846c0effb721b676987f9bedece5fea0
Vishal <vish.shinde@proton.me>
2024-01-30 07:45:27 +0530

    fixed delete journal

--- routes/delete-journal.js
+++ routes/delete-journal.js
@@ -4,9 +4,7 @@ const Journal = require('../models/journal')
 router.delete('/:id', async (req, res) => {
     try {
     await Journal.findByIdAndDelete(req.params.id)
-    // res.redirect('/journals')
     res.status(200).send();
-    res.json("Journal Deleted")
     } catch (error) {
         console.log(error)
         res.status(500)
